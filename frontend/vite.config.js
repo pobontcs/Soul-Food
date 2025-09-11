8f143c72ecf394c44bc2e0902c38bf542c1140e2
pobontcs <alhossainxx@gmail.com>
2025-09-11 12:19:29 +0600

    save

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: { '/api':"http://localhost:5000",},
+  base: '/', 
+  build: {
+    outDir: 'dist' // build folder
   },
-});
+  server: {
+    open: true,
+    proxy: {
+      '/api': 'http://localhost:5000', // dev proxy
+    },
+  },
+})
